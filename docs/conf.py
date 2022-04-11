db103afe4425ea978f4ccefe1c8bbb09a959e7e7
Elliot Palmer <elliot@ecoplumbers.com>
2022-04-11 17:34:22 -0400

    one last try before heading home

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../servicepytan'))
+sys.path.insert(0, os.path.abspath(os.path.join('..','servicepytan')))
 
 
 # -- Project information -----------------------------------------------------
