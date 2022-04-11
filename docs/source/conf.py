50f299afd150a8c2329f0a756fe7079cf030363f
Elliot Palmer <elliot@ecoplumbers.com>
2022-04-11 09:50:15 -0400

    add requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,6 +58,6 @@ html_theme = 'sphinx_rtd_theme'
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../servicepytan'))
+sys.path.insert(0, os.path.abspath('./../servicepytan'))
 
 autodoc_member_order = 'bysource'
