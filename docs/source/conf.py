c7534e3208b29510231b4eff89663321dad873b6
Elliot Palmer <palm1221ks@gmail.com>
2022-04-10 23:33:24 -0400

    one more time

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,6 +58,6 @@ html_theme = 'sphinx_rtd_theme'
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 autodoc_member_order = 'bysource'
