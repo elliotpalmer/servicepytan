e14099703d9eebbe94d66bfb01c23b0d9fd0e841
Elliot Palmer <palm1221ks@gmail.com>
2022-10-25 13:47:48 -0400

    add reporting class

---
+++ servicepytan/reports.py
@@ -0,0 +1,19 @@
+from servicepytan.utils import request_json, check_default_options, endpoint_url
+
+class Report:
+  """Primary class for retrieving Reporting Endpoint Data.
+
+  Attributes:
+      config_file: a string file path to the config file.
+  """
+  def __init__(self, category, report_id, config_file="servicepytan_config.json"):
+    """Inits DataService with configuration file and authentication settings."""
+    self.config_file = config_file
+    self.timezone = get_timezone_by_file(config_file)
+    self.category = category
+    self.report_id = report_id
+
+  def get_data(self, params):
+    """get report data"""
+    url = endpoint_url("reporting",f"report-category/{self.category}/reports/{self.report_id}/data")
+    return request_json(url, params=params, config_file=self.config_file, request_type="POST")
